860ded1179292011ff5017db386b4eb4f47abb89
Mateus Henrique <mateus.henrique@acad.ufsm.br>
2022-03-31 02:24:33 -0300

    Setup the logic in the controllers for CRUD operations

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,17 +1,40 @@
-const getAllTasks = (req,res) => {
+const Task = require('../models/Task');
+
+const getAllTasks = async (req,res) => {
+    try {
+        const tasks = await Task.find({});
+        res.status(200).json({ tasks });
+    } catch (error) {
+        console.error(error.errors.name.properties.message);
+        res.status(500).json({msg: error.errors.name.properties.message});
+    }
     res.send('all the tasks');
 }
 
-const createTask = (req,res) => {
-    console.log(req.body);
-    res.send('the task has been saved');
+const createTask = async (req,res) => {
+    try {
+        const task = await Task.create(req.body);
+        res.status(201).json({task});
+    } catch(e){
+        console.error(e.errors.name.properties.message);
+    }
 }
 
 
-const getTask = (req,res) => {
-    //const {id} = req.params;
-    //console.log(id);
-    res.send('get single task');
+const getTask = async (req,res) => {
+    
+    try {
+        const {id: taskID} = req.params;
+        const task = await Task.findOne({_id: taskID});
+
+        if(!task){
+            return res.status(404).json({msg: `no task with id: ${taskID}`});
+        }
+
+        res.status(200).json({ task });
+    } catch (error) {
+        res.status(500).json({msg: error});
+    }
 }
 
 const updateTask = (req,res) => {
